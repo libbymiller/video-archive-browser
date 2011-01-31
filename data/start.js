6b408420be1e5fc5ff0c27fdae28afc365a0d0aa
Libby Miller <libby@nicecupoftea.org>
2011-01-31 23:26:54 +0000

    Adding in various missing pieces so people can try it on a different host. Don't know if it'll work.

---
+++ data/start.js
@@ -0,0 +1,91 @@
+recommendations({
+  "programme_number": "START",
+  "suggestions": [
+    {
+      "image":"http://www.bbc.co.uk/iplayer/images/episode/b00kj2r2_303_170.jpg",
+      "programme_number": "FKIE541K",
+      "core_title": "BILL BAILEY'S REMARKABLE GUIDE TO THE ORCHESTRA",
+      "description": "Bill Bailey joins forces with Anne Dudley and the BBC Concert Orchestra for an evening of his music and comedy.",
+      "categories": [
+        {
+          "term_frequency": 94,
+          "term_name": "BBC CONCERT ORCHESTRA",
+          "term_number": "785.11BBC CONCERT"
+        }
+      ],
+      "series_title": "",
+      "crid": "crid://fp.bbc.co.uk/240XQL"
+    },
+    {
+      "programme_number": "ENRA026Y",
+      "core_title": "TRAINSPOTTING",
+
+      "description": "Director Danny Boyle, screenwriter John Hodge and producer Andrew Macdonald are joined by Ewan McGregor, Ewen Bremner, Peter Mullin, Johnny Lee Miller and author Irvine Welsh in giving their thoughts on the movie.",
+
+      "image":"http://www.bbc.co.uk/iplayer/images/episode/b00h9mz8_303_170.jpg",
+      "categories": [
+        {
+          "term_frequency": 26,
+          "term_name": "BRITISH FILM INDUSTRY",
+          "term_number": "791.41(42)"
+        }
+      ],
+      "series_title": "MOVIE CONNECTIONS",
+      "crid": "crid://fp.bbc.co.uk/1RQZMA"
+    },
+
+    {
+      "programme_number": "NCFV083L",
+      "core_title": "VOYAGE OF THE DAMNED",
+
+      "description":"When disaster hits the Titanic, the Doctor uncovers a threat to the whole human race.",
+
+      "image":"http://www.bbc.co.uk/iplayer/images/episode/b008lyb2_303_170.jpg",
+      "categories": [
+        {
+          "term_name": "ALIENS (EXTRA TERRESTRIAL)",
+          "term_freqency": 47,
+          "term_number": "523.07.007"
+        },
+        {
+          "term_name": "EARTH (ASTRONOMY)",
+          "term_freqency": 38,
+          "term_number": "525"
+        },
+        {
+          "term_name": "TIME TRAVEL",
+          "term_freqency": 133,
+          "term_number": "910:529"
+        }
+      ],
+      "series_title": "DR WHO",
+      "crid": "crid://fp.bbc.co.uk/54BXKZ"
+    },
+    {
+      "programme_number": "LDHB120T",
+      "core_title": "BRUNEL",
+
+      "description":"Jeremy Clarkson follows in the footsteps of the great engineer Isambard Kingdom Brunel whose designs for bridges, railways, steamships, docks and buildings revolutionised modern engineering.",
+
+      "image":"http://www.bbc.co.uk/iplayer/images/episode/b0074sf7_303_170.jpg",
+      "categories": [
+        {
+          "term_name": "SNOWDONIA (GWYNEDD)",
+          "term_freqency": 14,
+          "term_number": "(429.12SNOWDONIA)"
+        },
+        {
+          "term_name": "SALTASH RAILWAY BRIDGE",
+          "term_freqency": 6,
+          "term_number": "624.23(282.242TAMAR,SALTASH)"
+        }
+      ],
+      "series_title": "GREAT BRITONS",
+      "crid": "crid://fp.bbc.co.uk/4CQA08"
+    }
+  ],
+  "core_title": "Browse Video Archives",
+  "series_title": " ",
+  "description":"Click on the links to the right to start browsing Redux",
+  "crid": ""
+})
